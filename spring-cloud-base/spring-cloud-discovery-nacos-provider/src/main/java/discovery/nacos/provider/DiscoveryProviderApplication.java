2c8cc14a3fd3eee3ffdc7ec8ff89605433fa4c6e
weiwei <weismart1@gmail.com>
2022-03-14 21:08:09 +0800

    Initial commit

---
+++ spring-cloud-base/spring-cloud-discovery-nacos-provider/src/main/java/discovery/nacos/provider/DiscoveryProviderApplication.java
@@ -0,0 +1,27 @@
+package discovery.nacos.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DiscoveryProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryProviderApplication.class, args);
+    }
+
+    @RestController
+    static class TestController {
+
+        @GetMapping("/echo")
+        public String echo(String name) {
+            return "provider:" + name;
+        }
+
+    }
+
+}
