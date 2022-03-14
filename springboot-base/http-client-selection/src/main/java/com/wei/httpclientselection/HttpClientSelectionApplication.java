8b427ada1906bd879e89f6297d541dd5634b593b
weiwei <weismart1@gmail.com>
2022-03-14 21:37:01 +0800

    fix：边实践边留坑 在最nacos的实践过程中，发现http client的问题

---
+++ springboot-base/http-client-selection/src/main/java/com/wei/httpclientselection/HttpClientSelectionApplication.java
@@ -0,0 +1,15 @@
+package com.wei.httpclientselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * TODO http client 最佳实践
+ * */
+@SpringBootApplication
+public class HttpClientSelectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpClientSelectionApplication.class, args);
+    }
+
+}
---
+++ springboot-base/http-client-selection/src/test/java/com/wei/httpclientselection/HttpClientSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.httpclientselection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientSelectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
