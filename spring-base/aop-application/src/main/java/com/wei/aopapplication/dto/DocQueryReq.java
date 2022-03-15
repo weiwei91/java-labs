476f0a235cc9c6dfa6b54cedc4830adaf16421cc
weiwei <weismart1@gmail.com>
2022-03-15 21:26:01 +0800

    feat：aop 实践 实现接口的日志和耗时打印

---
+++ spring-base/aop-application/src/main/java/com/wei/aopapplication/dto/DocQueryReq.java
@@ -0,0 +1,8 @@
+package com.wei.aopapplication.dto;
+
+public class DocQueryReq extends PageReq {
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
