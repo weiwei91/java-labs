59e6c17ce53abba5ec5542b5d6f68f7e4b8e3a30
weiwei10 <weismart1@163.com>
2022-03-16 14:52:24 +0800

    feat: 设置id生成策略

--- java-base/common/src/main/java/com/wei/common/domain/entity/Device.java
+++ java-base/common/src/main/java/com/wei/common/domain/entity/Device.java
@@ -1,5 +1,7 @@
 package com.wei.common.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,16 @@ import java.util.Date;
 @Builder
 public class Device {
     //设备相关
+    /***
+     *     设置id生成策略
+     *     AUTO(0),//自增
+     *     NONE(1),//未设置主键
+     *     INPUT(2),//手动输入,需要自己配置id
+     *     ID_WORKER(3),//默认的全局唯一id
+     *     UUID(4),//全球唯一id
+     *     ID_WORKER_STR(5);//ID_WORKER字符串表示
+     * **/
+    @TableId(type= IdType.UUID)
     private String indexCode;
     private String ip;
     private String port;
