1a1426579c8d3ce5e9843cb634b26c94d5f63090
weiwei <weismart1@gmail.com>
2022-03-20 14:09:52 +0800

    fix：todofeign问题有待解决

--- java-base/concurrency-practice/src/main/java/com/wei/concurrencypractice/ConcurrencyPracticeApplication.java
+++ java-base/concurrency-practice/src/main/java/com/wei/concurrencypractice/ConcurrencyPracticeApplication.java
@@ -2,9 +2,11 @@ package com.wei.concurrencypractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class ConcurrencyPracticeApplication {
 
 
--- java-base/concurrency-practice/src/test/java/com/wei/concurrencypractice/ConcurrencyPracticeApplicationTests.java
+++ java-base/concurrency-practice/src/test/java/com/wei/concurrencypractice/ConcurrencyPracticeApplicationTests.java
@@ -2,6 +2,8 @@ package com.wei.concurrencypractice;
 
 import com.wei.common.base.response.Response;
 import com.wei.common.model.entity.Place;
+import com.wei.concurrencypractice.feign.DemoProviderFeignClient;
+import com.wei.concurrencypractice.feign.DeviceFeignClient;
 import com.wei.concurrencypractice.retrofit.DeviceServiceClient;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -14,10 +16,28 @@ class ConcurrencyPracticeApplicationTests {
 
     @Autowired
     DeviceServiceClient deviceServiceClient;
+    @Autowired
+    DeviceFeignClient deviceFeignClient;
+    @Autowired
+    DemoProviderFeignClient demoProviderFeignClient;
 
     @Test
     void contextLoads() {
         Response<Place> response = deviceServiceClient.getPlaceById("200");
+        log.info("1");
+
+    }
+
+    @Test
+    void feign() {
+        Response<Place> response = deviceServiceClient.getPlaceById("200");
+        log.info("1");
+
+    }
+
+    @Test
+    void feign1() {
+        String a = demoProviderFeignClient.echo("123");
         log.info("1");
 
     }
