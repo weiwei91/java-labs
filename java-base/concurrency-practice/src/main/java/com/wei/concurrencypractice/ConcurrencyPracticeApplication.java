8d4a4c93dde8ca5ea8f960ff20721e81d3e4ae56
weiwei <weismart1@gmail.com>
2022-03-20 13:56:23 +0800

    fix：服务提供者加入服务发现

--- java-base/concurrency-practice/src/main/java/com/wei/concurrencypractice/ConcurrencyPracticeApplication.java
+++ java-base/concurrency-practice/src/main/java/com/wei/concurrencypractice/ConcurrencyPracticeApplication.java
@@ -1,12 +1,8 @@
 package com.wei.concurrencypractice;
 
-import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class ConcurrencyPracticeApplication {
