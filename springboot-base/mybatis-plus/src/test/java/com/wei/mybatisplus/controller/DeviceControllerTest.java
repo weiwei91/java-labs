a42b88f373bdda87ff9c582a2403ed17946076eb
weiwei10 <weismart1@163.com>
2022-03-23 10:18:20 +0800

    fix : controller层单元测试

---
+++ springboot-base/mybatis-plus/src/test/java/com/wei/mybatisplus/controller/DeviceControllerTest.java
@@ -0,0 +1,46 @@
+package com.wei.mybatisplus.controller;
+
+import com.wei.common.model.entity.Device;
+import com.wei.mybatisplus.service.DeviceService;
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DeviceControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private DeviceService deviceService;
+
+    @Test
+    public void testGet() throws Exception {
+        // Mock UserService 的 get 方法
+        Mockito.when(deviceService.getById("weiwei")).thenReturn(
+                Device.builder().ip("10.12.13.10").indexCode("weiwei").deviceName("weiwei").build());
+
+        // 查询设备
+        ResultActions resultActions = mvc.perform(MockMvcRequestBuilders.get("/device/getDeviceById?id=weiwei"));
+
+        // 校验响应状态码
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk()); // 响应状态码 200
+
+        //逐个字段匹配
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("data.indexCode", IsEqual.equalTo("weiwei")));
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("data.ip", IsEqual.equalTo("10.12.13.10")));
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("data.deviceName", IsEqual.equalTo("weiwei")));
+    }
+
+}
