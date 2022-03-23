7f46f7dca916187f948129704b1200c2d771a2d7
weiwei10 <weismart1@163.com>
2022-03-23 10:51:49 +0800

    fix : service层单元测试

--- springboot-base/mybatis-plus/src/main/java/com/wei/mybatisplus/service/DeviceService.java
+++ springboot-base/mybatis-plus/src/main/java/com/wei/mybatisplus/service/DeviceService.java
@@ -2,8 +2,13 @@ package com.wei.mybatisplus.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.wei.common.model.entity.Device;
+import com.wei.mybatisplus.mapper.DeviceMapper;
 import org.springframework.stereotype.Service;
 
-@Service
+import javax.annotation.Resource;
+
 public interface DeviceService extends IService<Device> {
+
+    public Device getDeviceForUnitTest(String id);
+
 }
