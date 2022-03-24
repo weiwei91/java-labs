29bf81a01df4775fe0c2e1fa1dab9ae6f81d2761
weiwei10 <weismart1@163.com>
2022-03-24 17:53:22 +0800

    fix : 学习插件

--- springboot-base/mybatis-plus/src/main/java/com/wei/mybatisplus/config/MybatisPlusConfig.java
+++ springboot-base/mybatis-plus/src/main/java/com/wei/mybatisplus/config/MybatisPlusConfig.java
@@ -2,12 +2,17 @@ package com.wei.mybatisplus.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.DynamicTableNameInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.IllegalSQLInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.wei.mybatisplus.util.RequestDataHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Map;
+import java.util.Random;
 
 @MapperScan("com.wei.mybatisplus.mapper")
 @EnableTransactionManagement
@@ -25,6 +30,26 @@ public class MybatisPlusConfig {
         mybatisPlusInterceptor.addInnerInterceptor(new IllegalSQLInnerInterceptor());
         //新增防止全表更新与删除插件
         mybatisPlusInterceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        //动态表明插件
+        DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
+        /**
+         * 通过动态表明插件可以实现分表
+         * 规则可以自己定义
+         * 但是获取参数的方式需要自己写,可以做到读写分离
+         * **/
+
+        dynamicTableNameInnerInterceptor.setTableNameHandler((sql, tableName) -> {
+            // 获取参数方法
+            Map<String, Object> paramMap = RequestDataHelper.getRequestData();
+            String sqlString = sql;
+            String year = "_2022";
+
+            return tableName + year;
+        });
+        mybatisPlusInterceptor.addInnerInterceptor(dynamicTableNameInnerInterceptor);
+
+
+
         return mybatisPlusInterceptor;
     }
 
