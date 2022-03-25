71f14b05cbe500fdf4445d236fcb9927c6e17157
weiwei10 <weismart1@163.com>
2022-03-25 10:01:38 +0800

    feat : 开发属于自己的starter

---
+++ springboot-base/weitools-spring-boot-starter/src/main/java/com/wei/weitoolsspringbootstarter/WeitoolsSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.wei.weitoolsspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeitoolsSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeitoolsSpringBootStarterApplication.class, args);
+    }
+
+}
