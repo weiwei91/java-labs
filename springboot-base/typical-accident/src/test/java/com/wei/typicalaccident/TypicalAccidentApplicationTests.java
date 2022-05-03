b3cfc2fbff0492761bb007cd46e82e3ee29fb865
weiwei <weismart1@gmail.com>
2022-05-03 15:46:17 +0800

    fix：增加模块

---
+++ springboot-base/typical-accident/src/test/java/com/wei/typicalaccident/TypicalAccidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.typicalaccident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypicalAccidentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
