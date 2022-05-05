7e42624aa1bc2cb2637f329e8380e52b5e225054
weiwei10 <weismart1@163.com>
2022-05-05 11:17:34 +0800

    fix: 文件上传

---
+++ java-base/design-patterns/src/test/java/com/wei/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
