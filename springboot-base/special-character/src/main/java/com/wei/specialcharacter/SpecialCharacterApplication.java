6df980a9baa0eb7972ed769c6da4a7c05c24411e
weiwei10 <weismart1@163.com>
2022-09-22 15:52:33 +0800

    feat: 搭建好自测环境

--- springboot-base/special-character/src/main/java/com/wei/specialcharacter/SpecialCharacterApplication.java
+++ springboot-base/special-character/src/main/java/com/wei/specialcharacter/SpecialCharacterApplication.java
@@ -1,19 +1,28 @@
 package com.wei.specialcharacter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication(scanBasePackages = "com.wei")
 @MapperScan("com.wei.common.mapper")
+@Slf4j
 public class SpecialCharacterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpecialCharacterApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpecialCharacterApplication.class, args);
+        Environment environment = context.getBean(Environment.class);
+        log.info("服务启动成功:http://localhost:" + environment.getProperty("local.server.port"));
+
+
+
     }
 
 
