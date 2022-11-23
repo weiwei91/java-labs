ea925ba44c59dfd43e26f32c4b0233b4794b303f
weiwei10 <weiwei10.hikvisiom.com.cn>
2022-11-23 20:53:13 +0800

    测试sourcetree提交
    Signed-off-by: weiwei10 <weiwei10.hikvisiom.com.cn>

---
+++ springboot-base/feign/src/main/java/com/wei/feign/client/TestFeignClient.java
@@ -0,0 +1,12 @@
+package com.wei.feign.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @Author weiwei
+ * @Date 2022/11/22 16:31
+ * @Description
+ **/
+@FeignClient()
+public interface TestFeignClient {
+}
